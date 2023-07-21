a1155d7c257290309447a45d440aa3875fffb748
Ricardo Airton Silva <ricardoairtonsilva@hotmail.com>
2023-07-21 14:50:23 +0100

    refactored keymaps and added maximize and equalize panes

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -3,24 +3,31 @@
 vim.g.mapleader=' '
 vim.g.maplocalleader = ' '
 
+local keymaps = vim.keymap
+
 -- Tab Switching Keybinds
-vim.keymap.set('n', '¬', 'gt')
-vim.keymap.set('n', '˙', 'gT')
+keymaps.set('n', '¬', 'gt')
+keymaps.set('n', '˙', 'gT')
 
 -- Line Moving Keybinds
-vim.keymap.set('n', '∆', ':m .+1<CR>==')
-vim.keymap.set('n', '˚', ':m .-2<CR>==')
-vim.keymap.set('i', '∆', '<Esc>:m .+1<CR>==gi')
-vim.keymap.set('i', '˚', '<Esc>:m .-2<CR>==gi')
-vim.keymap.set('v', '∆', "'>+1<CR>gv=gv")
-vim.keymap.set('v', '˚', "'<-2<CR>gv=gv")
+keymaps.set('n', '∆', ':m .+1<CR>==')
+keymaps.set('n', '˚', ':m .-2<CR>==')
+keymaps.set('i', '∆', '<Esc>:m .+1<CR>==gi')
+keymaps.set('i', '˚', '<Esc>:m .-2<CR>==gi')
+keymaps.set('v', '∆', "'>+1<CR>gv=gv")
+keymaps.set('v', '˚', "'<-2<CR>gv=gv")
 
-vim.keymap.set('n', '<leader>r', ':so ~/.config/nvim/init.lua<CR>')
+-- Maximizes and Minimizes current window with Alt-m and Alt-=
+keymaps.set('n', 'µ', '<C-W>_<C-W><Bar>');
+keymaps.set('n', '≠', '<C-W><kEqual>');
+
+-- Reloads Config
+keymaps.set('n', '<leader>r', ':so ~/.config/nvim/init.lua<CR>')
 
 -- Diagnostics mappings
-vim.keymap.set('n', '<leader>do', ':lua vim.diagnostic.open_float()<CR>')
-vim.keymap.set('n', '<leader>d[', ':lua vim.diagnostic.goto_prev()<CR>')
-vim.keymap.set('n', '<leader>d]', ':lua vim.diagnostic.goto_next()<CR>')
-vim.keymap.set('n', '<leader>dd', ':Telescope diagnostics<CR>')
+keymaps.set('n', '<leader>do', ':lua vim.diagnostic.open_float()<CR>')
+keymaps.set('n', '<leader>d[', ':lua vim.diagnostic.goto_prev()<CR>')
+keymaps.set('n', '<leader>d]', ':lua vim.diagnostic.goto_next()<CR>')
+keymaps.set('n', '<leader>dd', ':Telescope diagnostics<CR>')
 
 
